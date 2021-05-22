c498d33d44b280040abfd0d9082fdd5742dab0da
Daniel Díaz <69810853+danield413@users.noreply.github.com>
2021-05-22 16:14:21 -0500

    prueba heroku 5

--- database/config.js
+++ database/config.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const dbConnection = async() => {
 
-    console.log(process.env.MONGODB_CNN);
     try {
         await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
